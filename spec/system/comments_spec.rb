3034a97bc1c4681b050f8e19abd093e9aa69ac30
zyonson <wakeup5216@gmail.com>
2022-07-30 18:11:37 +0900

    store-showページレイアウト変更

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -3,14 +3,12 @@ require 'rails_helper'
 RSpec.describe "Comments", type: :system do
   let(:user) { FactoryBot.create(:user) }
   let(:store) { FactoryBot.create(:store) }
-  
-  
-  
+
   before do
     sign_in_as(user)
     driven_by(:rack_test)
   end
-  
+
   scenario "check the comment function" do
     visit store_path(store)
     fill_in "comment[comment_title]", with: "最高"
@@ -24,6 +22,4 @@ RSpec.describe "Comments", type: :system do
     fill_in "comment[comment_content]", with: "安い"
     expect(page).to have_content "コメント数0件"
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
