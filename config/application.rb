504a3e1542eaf650fe2550a766641f3a7d607532
zyonson <wakeup5216@gmail.com>
2022-08-05 01:35:03 +0900

    form_forをform_withに修正

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module WindApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
   end
